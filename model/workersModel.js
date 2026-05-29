25ae2a04b5ff86135e7988e54f4fdf80d969af3f
Fran-cis123 <enemalifrancis123@gmail.com>
2026-05-29 22:01:50 +0100

    Complete workers onboarding profile update

--- model/workersModel.js
+++ model/workersModel.js
@@ -1,70 +1,66 @@
 import mongoose from "mongoose";
 
 const workerSchema = new mongoose.Schema({
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true,
-        unique: true
-    },
-    firstName: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    lastName: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    phoneNumber: {
-        type: String,
-        required: true,
-    },
-    specialty: {
-        type: String,
-        enum: ["nurse", "doctor", "therapist", "technician", "other"],
-        required: true
-    },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+    unique: true
+  },
 
-    experienceYears: {
-        type: Number,
-        default:0
-    },
+  firstName: {
+    type: String,
+    required: true,
+    trim: true
+  },
 
-    bio:{
-        type: String,
-        trim: true
-    },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true
+  },
 
-    
-    credentials: {
-        type: String,
-        documentName: String,
-        documentUrl: String,
-        uploadedAt: {
-            type: Date,
-            default: Date.now
-        }
-    },
-    isVerified: {
-        type: Boolean,
-        default: false
-    },
+  phoneNumber: {
+    type: String,
+    required: true
+  },
 
-    verificationStatus:{
-        type: String,
-        enum: ["Pending", "Approved", "Rejected"],
-        default: "Pending"
-    },
+  specialty: {
+    type: String,
+    enum: ["nurse", "doctor", "therapist", "technician", "other"],
+    required: true
+  },
 
-    certificationName: [{
-        documentName:String,
-        documentUrl: String,
-        expiryDate: Date
-    }],
-},{
-    timestamps: true
+  experienceYears: {
+    type: Number,
+    default: 0
+  },
+
+  bio: {
+    type: String,
+    trim: true
+  },
+
+  address:{
+    type:String
+  },
+  
+  certifications: [
+    {
+      documentName: String,
+      documentUrl: String,
+      expiryDate: Date
+    }
+  ],
+
+  verificationStatus: {
+    type: String,
+    enum: ["Pending", "Approved", "Rejected"],
+    default: "Pending"
+  }
+
+}, {
+  timestamps: true
 });
 
 export default mongoose.model("Worker", workerSchema);
